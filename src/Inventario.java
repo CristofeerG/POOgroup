cd9d386ae49289c83a173f7e3c0d700ae5832126
DayanaTigua86 <99457251+DayanaTigua86@users.noreply.github.com>
2022-02-10 22:57:15 -0500

    Update Inventario.java
    Agregué un método para ver los libros añadidos, un método para vender libros y un método para almacenar.

--- src/Inventario.java
+++ src/Inventario.java
@@ -1,4 +1,38 @@
 
-public class Inventario {
+import java.util.ArrayList;
 
+public class Inventario {
+	
+	private ArrayList<Libro> libros = new ArrayList<Libro>();
+	
+	public void almacenar(Libro libro) {
+		libros.add(libro);
+	}
+	
+	public Libro getLibrobyIsbn(String isbn) {
+		for(Libro l: libros) {
+			if(l.getIsbn().equals(isbn)) {
+				return l;
+			}
+		}
+		return null;
+	}
+	
+	public void verLibreria() {
+		for(Libro lb: libros) {
+			System.out.println(lb);
+		}
+	}
+
+	public ArrayList<Libro> getLibros() {
+		return libros;
+	}
+	
+	public void ventaLibro(Libro libro, int cantidad) {
+		if(libro.getCantidad()<cantidad) {
+			System.out.println("No disponemos en stock, lo sentimos.");
+		}else if(libro.getCantidad()>=cantidad) {
+			libro.setCantidad(libro.getCantidad()-cantidad);
+		}
+	}
 }
